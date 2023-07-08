87d22b7128a09e4afbe47f462cc783073e31792c
Eduardo da Costa Valadão <43937108+eduvaladao@users.noreply.github.com>
2023-07-07 21:30:51 -0300

    Add files via upload

--- plots_paper/setup.py
+++ plots_paper/setup.py
@@ -1,9 +1,4 @@
 from distutils.core import setup
-from Cython.Build import cythonize
-
-setup(ext_modules=cythonize("arcs.pyx")) 
-
-from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
 from Cython.Distutils import build_ext
@@ -12,14 +7,11 @@ import numpy
 ext_modules=[
     Extension("arcs",        ["arcs.pyx"],        include_dirs=[numpy.get_include()], extra_compile_args=["-O3"]),
     Extension("angles",      ["angles.pyx"],      include_dirs=[numpy.get_include()], extra_compile_args=["-O3"]),
-    Extension("lengths",     ["lengths.pyx"],     include_dirs=[numpy.get_include()], extra_compile_args=["-O3"]),    
+    Extension("lenghts",     ["lenghts.pyx"],     include_dirs=[numpy.get_include()], extra_compile_args=["-O3"]),    
     Extension("widths",      ["widths.pyx"],      include_dirs=[numpy.get_include()], extra_compile_args=["-O3"]),
     Extension("LWcross",     ["LWcross.pyx"],     include_dirs=[numpy.get_include()], extra_compile_args=["-O3"]),
 ]
 
-for e in ext_modules:
-    e.cython_directives = {'language_level': "3"} # all are Python-3
-    
 setup(
   name = 'Elliptical Sources',
   cmdclass = {'build_ext': build_ext},
